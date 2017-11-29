e6b30eef5bfa7daf795fc2819d27f686644fa3ae
Infi-Knight <soniravi829@gmail.com>
2017-11-29 17:13:20 +0530

    Initial commit

---
+++ lib/cards.ex
@@ -0,0 +1,15 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  create_cards
+  """
+  # In elixir there is an implicit return
+  # i.e result of last line is automatically returned
+  # you can use single quotes
+  def create_cards do
+    ["Ace", "Two", "Three"]
+  end
+end
